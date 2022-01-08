84d83bd3379857d149af5a77c3284e33d5c05b93
Mir Irshad <77124143+irshadmir312@users.noreply.github.com>
2022-01-08 10:58:06 +0530

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# code bY MIR IRSHAD KASHMIRI
+# FOLLOW ME ON INSTAGRAM
+
+echo -e "\e[34m  __  __ _____ _____  _ \e[0m\e[91m    _   _    _    ____ _  __ \e[0m"
+echo -e "\e[34m |  \/  | ____|_   _|/ \ \e[0m\e[91m  | | | |  / \  / ___| |/ / \e[0m"
+echo -e "\e[34m | |\/| |  _|   | | / _ \ \e[0m\e[91m | |_| | / _ \| |   | ' / \e[0m"
+echo -e "\e[34m | |  | | |___  | |/ ___ \ \e[0m\e[91m|  _  |/ ___ | |___| . \ \e[0m"
+echo -e "\e[34m |_|  |_|_____| |_/_/   \_ \e[0m\e[91m|_| |_/_/   \_\____|_|\_\ \e[0m"
+echo ""
+echo -e "CODE DEVELOPED BY \e[40;38;5;82m MIR \e[30;48;5;82m IRSHAD \e[0m"
