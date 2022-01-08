d5ec949db24eec11730693187a16fe45834958f4
Mir Irshad <77124143+irshadmir312@users.noreply.github.com>
2022-01-08 11:03:49 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#code by Irshad kashmiri
+#follow me on instagram
+#https://instagram.com/irshadmir312?utm_medium=copy_link
+echo ""
+bash logo.sh
+bash about.sh
+sleep 3
+bash load.sh
+echo ""
+echo ""
+sleep 3
+xdg-open https://instagram.com/irshadmir312?utm_medium=copy_link
+echo " PLEASE WAIT 20 SECOND "
+sleep 20
+clear
+apt update -y
+apt upgrade -y
+pkg intall php -y
+pkg inatall curl -y
+clear
+sleep 1
+xdg-open https://instagram.com/irshadmir312?utm_medium=copy_link
+echo " wait for 15 seconds "
+echo ""
